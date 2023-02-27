8fd3183d0ee063a8eae23af4f4299b98a9278ec0
Melissa <pg23melissa@vfs.com>
2023-02-26 23:08:36 -0600

    attempt

--- src/routes/ChartsView.vue
+++ src/routes/ChartsView.vue
@@ -16,7 +16,9 @@ Copyright (c) 2023. Melissa Osorio Inc. All Rights Reserved.
                 name:"myController Test",
             }
             this.props = {
-                name: String,
+                name: 'chart',
+                chartList: ['Bar Chart', 'Line Chart', 'Pie Chart', 'Heatmap'],
+                visible: true,
             }
         }
     }
@@ -25,8 +27,27 @@ Copyright (c) 2023. Melissa Osorio Inc. All Rights Reserved.
 
 </script>
 <template>
-    <section>
-        <h3>halo {{name}}</h3>
-        <VFSNavbar/>
+    <section class="flexbox columns chart">
+        <button @click="visible = !visible">Hide All</button>
+        <div v-for="name, i in chartList" :key="i">
+            <div v-if="name == 'Bar Chart'">
+                <VFSBarChart :title="name" v-show="visible"/>
+            </div>
+            
+        </div>
     </section>
 </template>
+
+<style scoped>
+/* Local styles for this template */
+    .chart {
+        /* display: block; */
+        margin:2vw;
+        border: 1px solid black;
+        background-color: lightgray;
+        color: black;
+        height: 78vh;
+        width: 80vw;
+    }
+
+</style>
